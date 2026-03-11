ffafb386bfd4891b51fd5e945e673431189fe334
ph1631-tech <ph1631@srmist.edu.in>
2026-03-11 15:33:00 +0530

    Added UC3

--- HelloApp.java
+++ HelloApp.java
@@ -1,13 +1,7 @@
 public class HelloApp {
     public static void main(String[] args) {
-        // Ensure a name is passed as command-line argument
-        if (args.length == 0) {
-            System.out.println("Hello, please provide your name as a command-line argument.");
-            return;
-        }
-
-        // Capture the first argument as the user's name
-        String userName = args[0];
+        // If no argument is passed, default to "World"
+        String userName = (args.length > 0) ? args[0] : "World";
         System.out.println("Hello " + userName + "!");
     }
 }
