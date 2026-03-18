718747f6d3dd306bb3dbed94a3728cb4fa4cfdf0
ph1631-tech <ph1631@srmist.edu.in>
2026-03-18 15:28:09 +0530

    Added UC5

--- HelloApp.java
+++ HelloApp.java
@@ -1,10 +1,11 @@
-public class HelloApp{
+public class HelloApp {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Hello World!");
+            System.out.println("Hello, World!");
         } else {
+            // Enhanced for loop (for-each loop)
             for (String name : args) {
-                System.out.println("Hello " + name + "!");
+                System.out.println("Hello, " + name + "!");
             }
         }
     }
