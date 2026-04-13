1cb1d44b279e8f19690ad1af4e02d489d2c55a51
ph1631-tech <ph1631@srmist.edu.in>
2026-04-13 22:28:27 +0530

    Added UC7

--- HelloApp.java
+++ HelloApp.java
@@ -1,15 +1,11 @@
-public class HelloApp {
+public class HelloApp{
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder greeting = new StringBuilder("Hello, ");
-            for (String name : args) {
-                greeting.append(name).append(", ");
-            }
-          
-            String finalGreeting = greeting.substring(0, greeting.length() - 2);
-            System.out.println(finalGreeting + "!");
+            // Efficiently join names with a comma and space
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
         }
     }
 }
